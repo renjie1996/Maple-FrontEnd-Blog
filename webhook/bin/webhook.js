fe1298b049661f7d2dcd5640552f3ff5df1b72bb
徐人杰 <2578370399@qq.com>
2018-01-26 21:23:02 +0800

    update webhook

--- webhook/bin/webhook.js
+++ webhook/bin/webhook.js
@@ -18,3 +18,33 @@ createServer((req, res) => {
 
 handler.on('error', err => console.error('Error:', err.message));
 handler.on('push', event => console.log(event));
+
+
+// var http = require('http')
+// var createHandler = require('github-webhook-handler')
+// var handler = createHandler({ path: '/webhook', secret: 'mytoken' })
+
+// http.createServer(function (req, res) {
+//   handler(req, res, function (err) {
+//     res.statusCode = 404
+//     res.end('no such location')
+//   })
+// }).listen(5151, () => console.log('listening on port 5151'))
+
+// handler.on('error', function (err) {
+//   console.error('Error:', err.message)
+// })
+
+// handler.on('push', function (event) {
+//   console.log('Received a push event for %s to %s',
+//     event.payload.repository.name,
+//     event.payload.ref)
+// })
+
+// handler.on('issues', function (event) {
+//   console.log('Received an issue event for %s action=%s: #%d %s',
+//     event.payload.repository.name,
+//     event.payload.action,
+//     event.payload.issue.number,
+//     event.payload.issue.title)
+// })
