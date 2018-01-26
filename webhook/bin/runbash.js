e92b0dd580e19f3491f94e3b49e2a7e44a7f2fa4
徐人杰 <2578370399@qq.com>
2018-01-27 01:41:10 +0800

    update webhook

--- webhook/bin/runbash.js
+++ webhook/bin/runbash.js
@@ -3,7 +3,7 @@ const { spawn } = require('child_process');
 function runCommand(cmd, args, callback) {
   console.log(cmd, args)
   const child = spawn( cmd, args )
-  const response = ''
+  let response = ''
   child.stdout.on('data', buffer => { response += buffer.toString() })
   child.stdout.on('end', () => { callback( response ) })
 };
