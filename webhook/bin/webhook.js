ea1324c3c14d62c3989f3a59b723f64181a53c3d
徐人杰 <2578370399@qq.com>
2018-01-26 21:52:46 +0800

    update webhook

--- webhook/bin/webhook.js
+++ webhook/bin/webhook.js
@@ -26,7 +26,7 @@ handler.on('push', event => {
   })
 });
 
-// issue
+// issue2
 handler.on('issues', event => {
   console.log('Received an issue event for %s action=%s: #%d %s',
     event.payload.repository.name,
